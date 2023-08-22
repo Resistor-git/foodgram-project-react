1d7a7c39ce571bfa42971f172711b5fddf1cdb1b
Resistor-git <partizan-resistor@yandex.ru>
2023-08-22 17:22:33 +0300

    Users authentication works. Created templates for some dockerfiles

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -20,6 +20,6 @@ router.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include('djoser.urls')),  # оставить или убрать ???
-    path('/auth/', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
