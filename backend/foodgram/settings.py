3fbd35d9fa2b722c24744eef3f80252f48b78419
Resistor-git <partizan-resistor@yandex.ru>
2023-09-23 12:20:05 +0300

    workflow, front fails

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/backend_static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = os.path.join(BASE_DIR / 'collected_static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
