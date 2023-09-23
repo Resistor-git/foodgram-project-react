51fcac33f587755a2993c9f460d981764c748992
Resistor-git <partizan-resistor@yandex.ru>
2023-09-23 17:44:43 +0300

    pictures not working

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9(-&g)10mveh-f)5^@aq$&=#$6^wc*jwgougp&5p=&f(%#q3%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
