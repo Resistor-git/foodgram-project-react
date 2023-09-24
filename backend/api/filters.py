f76687c9a80dc60f2ca50406b47f67ac9b77a667
Resistor-git <partizan-resistor@yandex.ru>
2023-09-24 18:05:33 +0300

    review-fix. change condition in filters to get rid of try/except

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -20,18 +20,14 @@ class RecipeFilter(django_filters.FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value:
-            try:
+            if self.request.user.is_authenticated:
                 return queryset.filter(favorites__user=self.request.user)
-            except TypeError:
-                return queryset
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value:
-            try:
+            if self.request.user.is_authenticated:
                 return queryset.filter(carts__user=self.request.user)
-            except TypeError:
-                return queryset
         return queryset
 
 
