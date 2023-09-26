6e3861ee0f7b2ff31a1c715d7e6de3cf4e6c976f
Resistor-git <partizan-resistor@yandex.ru>
2023-09-26 20:22:04 +0300

    review-fix-2. Refactored some functions in serializers (got rid of ifs)

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -47,9 +47,7 @@ class CustomUserRetrieveSerializer(UserSerializer):
         Anonymous user can not subscribe, so func returns False in this case.
         """
         current_user = self.context.get('request').user
-        if current_user.is_authenticated:
-            return obj.following.filter(user=current_user).exists()
-        return False
+        return current_user.is_authenticated and obj.following.filter(user=current_user).exists()
 
     class Meta:
         model = User
@@ -238,9 +236,7 @@ class RecipeListRetrieveSerializer(serializers.ModelSerializer):
           so func returns False in this case.
           """
         current_user = self.context.get('request').user
-        if current_user.is_authenticated:
-            return obj.favorites.filter(user=current_user).exists()
-        return False
+        return current_user.is_authenticated and obj.favorites.filter(user=current_user).exists()
 
     def get_is_in_shopping_cart(self, obj):
         """
@@ -250,9 +246,7 @@ class RecipeListRetrieveSerializer(serializers.ModelSerializer):
            so func returns False in this case.
            """
         current_user = self.context.get('request').user
-        if current_user.is_authenticated:
-            return obj.carts.filter(user=current_user).exists()
-        return False
+        return current_user.is_authenticated and obj.carts.filter(user=current_user).exists()
 
     class Meta:
         model = Recipe
